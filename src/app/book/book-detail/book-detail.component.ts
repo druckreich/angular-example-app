31f4cfe7ca81367ff91179ee0d411d7a8d10d775
Stefan Martinek <stefan.martinek@atuin.media>
2018-02-01 16:25:11 +0100

    dded layout

--- src/app/book/book-detail/book-detail.component.ts
+++ src/app/book/book-detail/book-detail.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {Store} from '@ngrx/store';
+import {Observable} from 'rxjs/Observable';
 import {Book} from '../../store/book/book.model';
-import {BookService} from '../../store/book/book.service';
+import * as fromRoot from '../../store/reducers';
 
 @Component({
   selector: 'app-book-detail',
@@ -10,15 +12,19 @@ import {BookService} from '../../store/book/book.service';
 })
 export class BookDetailComponent implements OnInit {
 
-  book: Book;
-
-  constructor(private activatedRoute: ActivatedRoute, private bookService: BookService) {
-    this.activatedRoute.queryParams.subscribe((params: any) => {
-      console.log(params);
-      bookService.getById(params['id']).subscribe((book: Book) => {
-        this.book = book;
+  book$: Observable<Book> =
+    Observable.combineLatest(
+      this.store.select(fromRoot.selectAllBooks),
+      this.route.params
+    )
+      .map((data: any) => {
+        const {0: books, 1: params} = data;
+        const id: number = +params['id'];
+        return books.find((b: Book) => b.id === id);
       });
-    });
+
+
+  constructor(private route: ActivatedRoute, private store: Store<any>) {
   }
 
   ngOnInit() {
